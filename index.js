9a3dcb3aaef4f7a98cef364aa6a70307f0f2f6c7
notbraison <orinabraison@gmail.com>
2024-02-21 01:48:41 +0300

    added tenants file, added tenants update capabilities to API

---
+++ index.js
@@ -0,0 +1,3 @@
+const excel = require('exceljs');
+
+
---
+++ tenantAPI.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+// Read the JSON file
+fs.readFile('tenants.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
+
+    // Parse JSON data
+    let tenants = JSON.parse(data);
+
+    // Update values
+    function updatePhone(n,p){
+    tenants.forEach(tenant => {
+        if (tenant.houseNumber === n) {
+            tenant.phoneNumber = p; // Update phone number for tenant with id 1
+        }
+    });    
+    }
+    
+
+    // Write updated data back to the file
+    fs.writeFile('tenants.json', JSON.stringify(tenants, null, 2), 'utf8', (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+            return;
+        }
+        console.log('File updated successfully');
+    });
+});
